d2da43f91c4ffdd6a2cc53c2ad19fa4634a9a8f4
victor-kithinji <kithinjivictor69@gmail.com>
2023-02-25 22:29:54 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const config = {
-no_ref: "off", //Control the HTTP referrer header, if you want to create an anonymous link that will hide the HTTP Referer header, please set to "on" .
-theme:"",//Homepage theme, use the empty value for default theme. To use urlcool theme, please fill with "theme/urlcool" .
-cors: "on",//Allow Cross-origin resource sharing for API requests.
+no_ref: "off",checkURL
+theme:"",
+cors: "on",
 unique_link:true,
 custom_link:false,
 }
@@ -10,7 +10,7 @@ const html404 = `<!DOCTYPE html>
 <body>
   <h1>404 Not Found.</h1>
   <p>The url you visit is not found.</p>
-  <a href="https://github.com/Victor-kithinji/URL_Shorter/" target="_self">Fork me on GitHub</a>
+  <a href="https://github.com/Victor-kithinji/URL_Shortner/" target="_self">Fork me on GitHub</a>
 </body>`
 
 let response_header={
@@ -27,8 +27,7 @@ if (config.cors=="on"){
 
 async function randomString(len) {
 　　len = len || 6;
-　　let $chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678';    /****默认去掉了容易混淆的字符oOLl,9gq,Vv,Uu,I1****/
-　　let maxPos = $chars.length;
+　　let $chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678';    
 　　let result = '';
 　　for (i = 0; i < len; i++) {
 　　　　result += $chars.charAt(Math.floor(Math.random() * maxPos));
@@ -147,7 +146,7 @@ async function handleRequest(request) {
   console.log(path)
   if(!path){
 
-    const html= await fetch("https://Victor-kithinji.github.io/URL_Shorter/"+config.theme+"/index.html")
+    const html= await fetch("https://Victor-kithinji.github.io/URL_Shortner/"+config.theme+"/index.html")
     
     return new Response(await html.text(), {
     headers: {
@@ -170,7 +169,7 @@ async function handleRequest(request) {
   if (location) {
     if (config.safe_browsing_api_key){
       if(!(await is_url_safe(location))){
-        let warning_page = await fetch("https://Victor-kithinji.github.io/URL_Shorter/safe-browsing.html")
+        let warning_page = await fetch("https://Victor-kithinji.github.io/URL_Shortner/safe-browsing.html")
         warning_page =await warning_page.text()
         warning_page = warning_page.replace(/{Replace}/gm, location)
         return new Response(warning_page, {
@@ -181,7 +180,7 @@ async function handleRequest(request) {
       }
     }
     if (config.no_ref=="on"){
-      let no_ref= await fetch("https://Victor-kithinji.github.io/URL_Shorter/no-ref.html")
+      let no_ref= await fetch("https://Victor-kithinji.github.io/URL_Shortner/no-ref.html")
       no_ref=await no_ref.text()
       no_ref=no_ref.replace(/{Replace}/gm, location)
       return new Response(no_ref, {
